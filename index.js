5d82d63b879fd3afffbd17f4000beba085f3b26d
Phillip Logan <philo3194@gmail.com>
2021-01-13 20:39:52 -0500

    switching back to telegraf

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//const Telegraf = require("telegraf");
-const { Composer } = require('micro-bot');
+const Telegraf = require("telegraf");
 const moment = require("moment-timezone");
 const mongoose = require("mongoose");
 const parse = require("parse-messy-time");
@@ -9,8 +8,7 @@ if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
-//const bot = new Telegraf(process.env.FAB_BOT_TOKEN);
-const bot = new Composer();
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.use(commandArgsMiddleware());
 
@@ -18,7 +16,7 @@ const Minder = mongoose.model("Minder", {
   text: String,
   who: String,
   id: String,
-  created: { type: String, default: Date.now }
+  created: { type: String, default: Date.now },
 });
 
 // bot.start(ctx => ctx.reply("Welcome!"));
@@ -30,65 +28,58 @@ const Minder = mongoose.model("Minder", {
 // bot.command("modern", ({ reply }) => reply("Yo"));
 // bot.command("hipster", Telegraf.reply("λ"));
 
-bot.command("day", ctx => {
+bot.command("day", (ctx) => {
   let firstDate = moment.tz("2019-07-14", "America/Jamaica");
 
-  let secondDate = ctx.state.args.length
-    ? moment.tz(
-        parse(ctx.state.args.join(" "))
-          .toISOString()
-          .split("T")[0],
-        "America/Jamaica"
-      )
-    : moment.tz("America/Jamaica");
+  let secondDate = ctx.state.args.length ? moment.tz(parse(ctx.state.args.join(" ")).toISOString().split("T")[0], "America/Jamaica") : moment.tz("America/Jamaica");
 
   let diffDays = secondDate.diff(firstDate, "days");
 
   ctx.reply(`${["brn", "anju", "fil"][diffDays % 3]}'s day`);
 });
 
-bot.command("pants", ctx => {
+bot.command("pants", (ctx) => {
   if (ctx.state.args.length) {
     if (ctx.state.args[0] === "on")
       ctx
         .reply("someone's in the house!")
-        .then(msg => ctx.pinChatMessage(ctx.message.chat.id, msg))
-        .catch(err => ctx.reply(err.message));
+        .then((msg) => ctx.pinChatMessage(ctx.message.chat.id, msg))
+        .catch((err) => ctx.reply(err.message));
     else if (ctx.state.args[0] === "off")
       ctx
         .reply("ok, you can come out now")
         .then(() => ctx.unpinChatMessage(ctx.message.chat.id))
-        .catch(err => ctx.reply(err.message));
+        .catch((err) => ctx.reply(err.message));
   } else {
     ctx.reply("/pants requires yes/no arg");
   }
 });
 
-bot.command("minders", async ctx => {
+bot.command("minders", async (ctx) => {
   try {
     await mongoose.connect(process.env.FAB_BOT_MONGO, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
     });
     let messages = await Minder.find({
       id: ctx.message.chat.id,
-      who: ctx.message.from.id
+      who: ctx.message.from.id,
     }).sort({ created: -1 });
-    messages.map(message => ctx.reply(message.text));
+    messages.map((message) => ctx.reply(message.text));
   } catch (err) {
     ctx.reply(err.message);
   }
 });
 
-bot.command("mindme", async ctx => {
+bot.command("mindme", async (ctx) => {
   try {
     await mongoose.connect(process.env.FAB_BOT_MONGO, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
     });
 
     const minder = await new Minder({
       text: ctx.state.args.join(" "),
       who: ctx.message.from.id,
-      id: ctx.message.chat.id
+      id: ctx.message.chat.id,
     });
 
     await minder.save();
@@ -98,5 +89,4 @@ bot.command("mindme", async ctx => {
   }
 });
 
-module.exports = bot
-
+bot.startWebhook("/", null, process.env.PORT);
