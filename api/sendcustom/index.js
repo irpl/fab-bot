f9a9665d25ff66b8cfc493caa6b3cb3aee921b19
Phillip Logan <phillip.logan@digicelgroup.com>
2022-01-30 16:55:01 -0500

    added optional pin

--- api/sendcustom/index.js
+++ api/sendcustom/index.js
@@ -6,5 +6,7 @@ module.exports = async (request, response) => {
   console.log("Incoming message", request.body);
 
   var res = await bot.telegram.sendMessage(request.body.chatId, request.body.message);
+
+  request.body.pin && (await bot.telegram.pinChatMessage(request.body.chatId, res.message_id));
   return await response.json(res);
 };
