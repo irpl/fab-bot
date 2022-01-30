c7687ea4f3ec2c1e4ceba151a7695021b0097841
Phillip Logan <phillip.logan@digicelgroup.com>
2022-01-30 13:58:13 -0500

    now?

--- api/sendcustom/index.js
+++ api/sendcustom/index.js
@@ -5,6 +5,6 @@ const bot = new Telegraf(process.env.FAB_BOT_TOKEN);
 module.exports = async (request, response) => {
   console.log("Incoming message", request.body);
 
-  return await bot.telegram.sendMessage(request.body.chatId, request.body.message).then(() => response.status(200));
+  return await bot.telegram.sendMessage(request.body.chatId, request.body.message).then(() => response.json());
   // return await response.status(200);
 };
