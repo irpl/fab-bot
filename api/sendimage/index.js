158f63e9a1bc1e095a2ef290c527903644621fcf
Phillip Logan <philo3194@gmail.com>
2023-06-12 18:32:12 -0500

    idk

--- api/sendimage/index.js
+++ api/sendimage/index.js
@@ -5,8 +5,8 @@ const bot = new Telegraf(process.env.FAB_BOT_TOKEN);
 
 module.exports = async (request, response) => {
   console.log("Incoming image");
-
+  const chatId = request.body.chatId;
   const screenshotBuffer = await buffer(request);
-  await bot.telegram.sendPhoto(request.params.id, { source: screenshotBuffer });
+  await bot.telegram.sendPhoto(chatId, { source: screenshotBuffer });
   return await response.json({ image: "sent" });
 };
